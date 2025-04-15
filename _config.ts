e05f2c7f22c493885708f00c8de0d9eede123dee
fxadilima <xaverius2017@gmail.com>
2025-04-15 16:19:19 +0700

    commited from command-line

--- _config.ts
+++ _config.ts
@@ -1,5 +1,21 @@
 import lume from "lume/mod.ts";
+import footnotes from 'markdown-it-footnote';
 
-const site = lume();
+const markdown = {
+  plugins: [footnotes]
+};
+
+const site = lume({
+  server: {
+    port: 3000
+  },
+  prettyUrls: true,
+  emptyDest: true,
+}, {markdown});
+
+site.copy("./images", "images");
+
+site.copy("./favicon.ico", "favicon.ico");
+site.copy("./favicon.png", "favicon.png");
 
 export default site;
